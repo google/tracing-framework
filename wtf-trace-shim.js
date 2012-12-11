548319feb8916793a90fd56cb8d0c0b794c48006
Ben Vanik <ben.vanik@gmail.com>
2012-12-11 12:33:11 -0800

    Adding @license to the API shim.

--- wtf-trace-shim.js
+++ wtf-trace-shim.js
@@ -1,8 +1,9 @@
 /**
+ * @license
+ * https://github.com/google/tracing-framework
  * Copyright 2012 Google, Inc. All Rights Reserved.
- *
  * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
+ * found at https://github.com/google/tracing-framework/blob/master/LICENSE.
  */
 
 /**
