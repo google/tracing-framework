83ed25e9494337131f4050c1cf8428d5ca0da4c5
Ryan Sturgell <rsturgell@google.com>
2013-03-07 20:00:57 -0800

    Fix accidental assignment in if condition.

--- bin/instrument.js
+++ bin/instrument.js
@@ -114,13 +114,9 @@ function transformCode(moduleId, url, sourceCode) {
     // foo = function() {};
     // Bar.foo = function() {};
     //
-    if (node.parent.type = 'AssignmentExpression') {
+    if (node.parent.type == 'AssignmentExpression') {
       // We are the RHS, LHS is something else.
       var left = node.parent.left;
-      if (!left) {
-        // This happens if it's (function(){})();
-        return null;
-      }
       if (left.type == 'MemberExpression') {
         // Bar.foo = function() {};
         // left.object {type: 'Identifier', name: 'Bar'}
@@ -137,7 +133,7 @@ function transformCode(moduleId, url, sourceCode) {
       return null;
     }
 
-    console.log('unknown fn construct', node);
+    //console.log('unknown fn construct', node);
 
     // TODO(benvanik): support jscompiler prototype alias:
     // _.$JSCompiler_prototypeAlias$$ = _.$something;
