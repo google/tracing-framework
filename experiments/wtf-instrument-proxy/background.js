a7d4a08cdd689b544abab96b842a34105c5d18a4
Ben Vanik <ben.vanik@gmail.com>
2013-04-05 12:54:08 -0700

    Making the instrumentation proxy ignore github and support HTTPS.

--- experiments/wtf-instrument-proxy/background.js
+++ experiments/wtf-instrument-proxy/background.js
@@ -28,6 +28,7 @@ function registerRules() {
       'https://www.google.com/jsapi',
       'https://apis.google.com/js/client.js',
       'http://google.github.com/tracing-framework/bin/',
+      'http://google.github.io/tracing-framework/bin/',
       'https://ssl.google-analytics.com/ga.js',
     ];
     for (var n = 0; n < whitelist.length; n++) {
@@ -40,9 +41,16 @@ function registerRules() {
     if (details.method == 'GET' &&
         (details.type == 'script' || details.type == 'xmlhttprequest') &&
         details.url.indexOf('blob:') != 0 &&
-        details.url.indexOf('http://localhost:8081/inject') != 0) {
+        details.url.indexOf('http://localhost:8081/inject') != 0 &&
+        details.url.indexOf('https://localhost:8082/inject') != 0) {
       console.log(details.url);
-      var localUrl = 'http://localhost:8081/inject?url=' + escape(details.url);
+      var localUrl;
+      if (details.url.indexOf('https://') == 0) {
+        localUrl = 'https://localhost:8082/inject';
+      } else {
+        localUrl = 'http://localhost:8081/inject';
+      }
+      localUrl += '?url=' + escape(details.url);
       return {
         redirectUrl: localUrl
       };
