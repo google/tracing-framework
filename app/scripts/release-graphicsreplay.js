9640e5b3ae05f41c6d93e34e2859f330a3f94367
Chi Zeng <zeng@college.harvard.edu>
2013-07-18 16:03:45 -0700

    Let the user specify the trace URL. Fixes #384.
    The user can now specify the trace URL in the GET parameters of the URL of the standalone graphics replay file.

--- app/scripts/release-graphicsreplay.js
+++ app/scripts/release-graphicsreplay.js
@@ -11,7 +11,13 @@
  * @author chizeng@google.com (Chi Zeng)
  */
 
+// Get the trace URL.
+var search = window.location.search;
+if (!search || !search.length || search.indexOf('?url=') != 0) {
+  throw new Error(
+      'No trace URL specified. Specify the trace URL after \'?url=\'.');
+}
+var traceUrl = search.substr(5);
 
 var parentElement = document.getElementById('graphicsReplayStagingArea');
-wtf.replay.graphics.setupStandalone(
-    '../private/traces/vectortown-fun.wtf-trace', parentElement);
+wtf.replay.graphics.setupStandalone(traceUrl, parentElement);
