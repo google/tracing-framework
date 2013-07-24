b55d6ff4c6590c4f92c1cf620561520d90ae0dff
Chi Zeng <zeng@college.harvard.edu>
2013-07-24 09:30:48 -0700

    Labeled canvases with context handles.
    Canvases displayed in the main pane have been labeled with the handles of their contexts.

--- src/wtf/replay/graphics/ui/contextbox.js
+++ src/wtf/replay/graphics/ui/contextbox.js
@@ -13,6 +13,7 @@
 
 goog.provide('wtf.replay.graphics.ui.ContextBox');
 
+goog.require('goog.dom');
 goog.require('goog.soy');
 goog.require('wtf.replay.graphics.ui.contextBox');
 goog.require('wtf.ui.Control');
@@ -23,14 +24,17 @@ goog.require('wtf.ui.Control');
  * Encapsulates a context and its canvas element.
  *
  * @param {!WebGLRenderingContext} context The context.
+ * @param {string} contextHandle The handle of the context.
  * @param {!Element} parentElement The parent element.
  * @param {goog.dom.DomHelper=} opt_domHelper The DOM Helper.
  * @constructor
  * @extends {wtf.ui.Control}
  */
 wtf.replay.graphics.ui.ContextBox = function(
-    context, parentElement, opt_domHelper) {
+    context, contextHandle, parentElement, opt_domHelper) {
   goog.base(this, parentElement, opt_domHelper);
+  this.createContextLabel_(contextHandle);
+
   /**
    * The context this box encapsulates.
    * @type {!WebGLRenderingContext}
@@ -54,11 +58,26 @@ wtf.replay.graphics.ui.ContextBox.prototype.createDom = function(dom) {
 
 
 /**
+ * Creates a label for the context.
+ * @param {string} contextHandle The handle of the context.
+ * @private
+ */
+wtf.replay.graphics.ui.ContextBox.prototype.createContextLabel_ = function(
+    contextHandle) {
+  var label = 'Context with Handle ' + contextHandle;
+  var labelElement = this.getChildElement(
+      goog.getCssName('replayGraphicsContextBoxHandleLabel'));
+  goog.dom.setTextContent(labelElement, label);
+};
+
+
+/**
  * Appends the context to the canvas box.
  * @private
  */
 wtf.replay.graphics.ui.ContextBox.prototype.appendCanvas_ = function() {
   this.getDom().appendChild(
-      this.getChildElement(goog.getCssName('replayGraphicsCanvasContainer')),
+      this.getChildElement(
+          goog.getCssName('replayGraphicsContextBoxCanvasContainer')),
       this.context_.canvas);
 };
