6163b6584e23e2f08e249fc96f28b5e4138557ab
njong <njong@google.com>
2016-02-01 21:04:21 -0800

    Build fixes for iOS and Android
    Replace strchrnul usage with strchr (and avoid implicit conversion from name_spec_ to intermediate std::string value).
    
    Adopt <chrono> for timing. (Note that steady_clock is not guaranteed to provide nanosecond resolution, but it does on Linux/Android and iOS, at least. And high_resolution_clock is not guaranteed to be monotonic.)

--- bindings/cpp/include/wtf/platform/platform_default_inl.h
+++ bindings/cpp/include/wtf/platform/platform_default_inl.h
@@ -1,20 +1,16 @@
 #ifndef TRACING_FRAMEWORK_BINDINGS_CPP_INCLUDE_WTF_PLATFORM_DEFAULT_INL_H_
 #define TRACING_FRAMEWORK_BINDINGS_CPP_INCLUDE_WTF_PLATFORM_DEFAULT_INL_H_
 
-#include <time.h>
+#include <chrono>
 
 namespace wtf {
 
 namespace internal {
 extern uint64_t base_timestamp_nanos;
-static const uint64_t kNanosecondsPerSecond = 1000000000;
 
 inline uint64_t GetNanoTime() {
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  return static_cast<uint64_t>(ts.tv_sec) * kNanosecondsPerSecond +
-         static_cast<uint64_t>(ts.tv_nsec);
+  auto duration = std::chrono::steady_clock::now().time_since_epoch();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
 }
 
 }  // namespace internal
